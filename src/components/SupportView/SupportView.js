02cf3532b3a0753fd961e85c36f1d216b168ad4c
Danny Yang <dannyyaaj@gmail.com>
2018-08-04 17:02:21 -0500

    set up component local state and connect to redux state

--- src/components/SupportView/SupportView.js
+++ src/components/SupportView/SupportView.js
@@ -1,13 +1,59 @@
 import React, { Component } from 'react'
-
+import { connect } from 'react-redux';
+import NextButton from '../NextButton/NextButton'
+import FeedbackForm from '../FeedbackForm/FeedbackForm';
 class SupportView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formTitle: `Do you feel supported by your Instructors and/or peers today?`,
+      actionType: 'STORE_SUPPORT',
+      supportResponse: '',
+      lowestScoreLabel: `1 - I feel abandoned`,
+      highestScoreLabel: `5 - I feel supported!`
+    }
+  }
+
+  handleChangeForResponse = (event) => {
+    this.setState({
+      ...this.state,
+      supportResponse: event.target.value
+    })
+  }
+
+  handleNextButton = (event) => {
+    event.preventDefault();
+    // check if user responded
+    switch (this.state.supportResponse) {
+      case undefined || null || '':
+        alert('Please select a response before moving on')
+        break;
+      default:
+        // send support response to redux
+        this.props.dispatch(
+          {
+            type: this.state.actionType,
+            payload: this.state.supportResponse
+          },
+          this.props.history.push('/4')
+        )
+    }
+  }
+
   render() {
     return (
       <div>
-        <h1>Support</h1>
+        <FeedbackForm
+          formTitle={this.state.formTitle}
+          view={this.state.supportResponse}
+          handleChangeForResponse={this.handleChangeForResponse}
+          lowestScoreLabel={this.state.lowestScoreLabel}
+          highestScoreLabel={this.state.highestScoreLabel}
+        />
+        <NextButton handleNextButton={this.handleNextButton} />
       </div>
     )
   }
 }
 
-export default SupportView
+export default connect()(SupportView);
