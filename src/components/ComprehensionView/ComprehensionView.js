e5402c2220f9d27e95a568e9bf7b0c1faad0380d
Danny Yang <dannyyaaj@gmail.com>
2018-08-04 13:35:32 -0500

    set up comprehension feedback view

---
+++ src/components/ComprehensionView/ComprehensionView.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ComprehensionView extends Component {
+  render() {
+    return (
+        <h1>Comprehension</h1>
+    )
+  }
+}
+
+export default ComprehensionView
