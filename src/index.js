85c7aec4eef46c30a8b1f6cd2053646e1b2f2494
Danny Yang <dannyyaaj@gmail.com>
2018-08-04 12:39:57 -0500

    set up redux store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {
+  id: undefined,
+  feeling: '',
+  comprehension: '',
+  support: '',
+  comments: ''
+}
+
+const feedbackResponse = (state = initialState, action) => {
+  switch (action.type) {
+    case 'STORE_FEELINGS':
+      return { ...state, feeling: action.payload }
+    case 'STORE_COMPREHENSION':
+      return { ...state, comprehension: action.payload }
+    case 'STORE_SUPPORT':
+      return { ...state, support: action.payload }
+    case 'STORE_COMMENTS':
+      return { ...state, comments: action.payload }
+    default:
+      return state
+  }
+}
+
+const storeInstance = createStore(
+  feedbackResponse,
+  applyMiddleware(logger)
+)
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
