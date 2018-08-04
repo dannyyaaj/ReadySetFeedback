8b992bc15cb3f9ddb218aa1460acb2122ce5b401
Danny Yang <dannyyaaj@gmail.com>
2018-08-04 13:57:26 -0500

    add button to redirect user to home page

--- src/components/ErrorNotFound/ErrorNotFound.js
+++ src/components/ErrorNotFound/ErrorNotFound.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
 
 class ErrorNotFound extends Component {
-  render() {
-    return (
 
-        <h2>404 page not found</h2>
+
+  // redirect user to home page
+  goHome = () => this.props.history.push('/');
+
+
+  render() {
+    console.log();
+    return (
+      <div>
+        <h1>404</h1>
+        <p>OOPS, SORRY WE CAN'T FIND THAT PAGE!</p>
+        <button onClick={this.goHome}>Home Page</button>
+      </div>
     )
   }
 }
