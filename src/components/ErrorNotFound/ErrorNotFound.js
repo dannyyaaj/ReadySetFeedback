f5dc6950937b3135402871b230c067303ad015e8
Danny Yang <dannyyaaj@gmail.com>
2018-08-04 13:44:24 -0500

    set up 404 error view

---
+++ src/components/ErrorNotFound/ErrorNotFound.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ErrorNotFound extends Component {
+  render() {
+    return (
+
+        <h2>404 page not found</h2>
+    )
+  }
+}
+export default ErrorNotFound
