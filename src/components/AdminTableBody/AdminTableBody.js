f58808b544c4d0f2a0647c307e9a17a02f713e6e
Danny Yang <dannyyaaj@gmail.com>
2018-08-05 17:45:49 -0500

    fix spacing and indentation

---
+++ src/components/AdminTableBody/AdminTableBody.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
+
+class AdminTableBody extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      feedbackResponses: []
+    }
+  }
+  componentDidMount() {
+    axios.get('/api/feedback')
+      .then(response => {
+        this.setState({
+          feedbackResponses: response.data
+        })
+      })
+      .catch(error => {
+        console.log(error);
+
+      })
+  }
+
+  render() {
+    const responsesToDisplay =
+      this.state.feedbackResponses.map((feedback, index) => {
+        return (
+          <TableRow key={index}>
+            <TableCell>{feedback.feeling}</TableCell>
+            <TableCell>{feedback.understanding}</TableCell>
+            <TableCell>{feedback.support}</TableCell>
+            <TableCell>{feedback.comments}</TableCell>
+          </TableRow>
+        )
+      })
+
+    return (
+      <TableBody>{responsesToDisplay}</TableBody>
+    )
+  }
+}
+
+export default AdminTableBody
