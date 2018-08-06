4ea33b9fda8c1ba69fca2b53478025010e99b272
Danny Yang <dannyyaaj@gmail.com>
2018-08-05 19:27:06 -0500

    make thank you button functional and redirect user to submit another response

--- src/components/ThankYouView/ThankYouView.js
+++ src/components/ThankYouView/ThankYouView.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 import ThankYouButton from '../ThankYouButton/ThankYouButton';
 
+
 class ThankYouView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+  handleThankYouButton = (event) => {
+    event.preventDefault();
+    this.props.history.push('/')
+  }
   render() {
     return (
       <div>
         <h1>Thank You!</h1>
-        <ThankYouButton />
+        <ThankYouButton
+        handleThankYouButton={this.handleThankYouButton}
+        />
       </div>
     )
   }
 }
 
-export default ThankYouView
+export default ThankYouView;
